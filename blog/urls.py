f8f672ed9ccba46d88df13246db2af6b94f1ee9c
kidellwq <kidell@163.com>
2018-07-16 22:51:36 +0800

    bb

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, detail
 
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('post/<int:pk>/', detail, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+import markdown
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', {
-        'title': '欢迎光临我的博客',
-        'welcome': '欢迎访问我的博客首页',
+        'post_list': post_list,
+    })
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body, extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.codehilite',
+                                    'markdown.extensions.toc',
+                                    ])
+    return render(request, 'blog/detail.html', {
+        'post': post,
     })
